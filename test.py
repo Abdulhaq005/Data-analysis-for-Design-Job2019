174ff4f493ed634be2452e738b6fb0dd0249b746
unknown <mohammadhaq01@gmail.com>
2020-11-29 15:40:58 +0100

    files adding

---
+++ test.py
@@ -0,0 +1,10 @@
+a = [[1,2,3,4],[1,2],[3,4],[2,4],[1,3]]
+
+b = []
+count = 0
+for i in a:
+    for j in range(len(i)):
+        if j % 2 != 0: 
+            b.append
+    count = count + 1
+print(b)
